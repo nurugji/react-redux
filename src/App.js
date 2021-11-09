58de65a90b8d9709d010c976c3d66b3d181e5bbb
pingu <nurugji@gmail.com>
2021-11-09 22:39:59 +0900

    3.2 connect state without redux

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import DisplayNumberRoot from "./components/DisplayNumberRoot";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    number: 0,
+  };
   render() {
     return (
       <div className="App">
         <h1>Root</h1>
-        <AddNumberRoot></AddNumberRoot>
-        <DisplayNumberRoot></DisplayNumberRoot>
+        <AddNumberRoot
+          onClick={(size) => {
+            this.setState({
+              number: this.state.number + size,
+            });
+          }}
+        ></AddNumberRoot>
+        <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
       </div>
     );
   }
